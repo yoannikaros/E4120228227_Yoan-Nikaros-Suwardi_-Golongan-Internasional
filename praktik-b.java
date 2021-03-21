ff0487a2780861028c612e577fdeeef09da0781e
Yoan Nikaros <75083157+yoannikaros@users.noreply.github.com>
2021-03-21 21:35:54 +0700

    Create praktik-b.java

---
+++ praktik-b.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktik;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Praktik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       String nama = "Yoan Nikaros Suwardi";
+       char abjad = 'y';
+       int a = 6;
+       double b =6.9;
+              
+System.out.println(nama);
+System.out.println(a);
+
+    }
+    
+}
